ca783c44ab460b1e81281ffa1e934444b1b45525
best2409 <best2409@naver.com>
2013-06-24 20:44:49 +0900

    hhhh

--- SpringProject/src/sample01/HelloApp.java
+++ SpringProject/src/sample01/HelloApp.java
@@ -12,9 +12,9 @@ public class HelloApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("sample01.xml"));
-		MessageBeanImpl bean2 = factory.getBean("messageBean", MessageBeanImpl.class);
+		MessageBeanImpl bean3 = factory.getBean("messageBean", MessageBeanImpl.class);
 		
-		bean2.sayHello();
+		bean3.sayHello();
 		
 		System.out.print((299/3f *10+0.5f) /10f);
 	}
