62ccc12c765c5c8d5ee1ab6672fdaec00053f95a
best2409 <best2409@naver.com>
2013-06-24 20:10:44 +0900

    hello app

---
+++ SpringProject/src/sample01/HelloApp.java
@@ -0,0 +1,22 @@
+package sample01;
+
+import org.springframework.beans.factory.*;
+import org.springframework.beans.factory.xml.*;
+import org.springframework.core.io.*;
+
+public class HelloApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("sample01.xml"));
+		MessageBeanImpl bean2 = factory.getBean("messageBean", MessageBeanImpl.class);
+		
+		bean2.sayHello();
+		
+		System.out.print((299/3f *10+0.5f) /10f);
+	}
+
+}
